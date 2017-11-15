fd202b5946b4fcfedcdc943d2764812175faf1a5
Jose Luis Bucio <jlbucio4@gmail.com>
2017-11-15 14:32:40 -0600

    Update lab12.ino

--- Course/lab12.ino
+++ Course/lab12.ino
@@ -1,1 +1,19 @@
+// 15 Nov 2017
 
+const byte ledPin = 11;
+const byte interruptPin = 2;
+volatile byte state = LOW;
+
+void setup(){
+  pinMode(ledPin, OUTPUT);
+  pinMode(interrupt, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
+}
+
+void loop(){
+  digitalWrite(ledPin, state);
+}
+
+void blink(){
+  state = !state;
+}
