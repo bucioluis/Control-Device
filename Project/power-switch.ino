7c9e9978e47277deda0c46b20ff7f7110908384d
Jose Luis Bucio <jlbucio4@gmail.com>
2017-11-28 22:00:21 -0600

    Update power-switch.ino

--- Project/power-switch.ino
+++ Project/power-switch.ino
@@ -1,1 +1,21 @@
 
+int buttonInput = 2;
+int lightOutput = 13;
+int buttonState = 0;
+
+void setup(){
+
+    pinMode(lightOutput, OUTPUT);
+    pinMode(buttonInput, INPUT);
+    digitalWrite(button, HIGH);
+}
+
+void loop(){
+  buttonState = digitalRead(buttonInput);
+  if(buttonState == HIGH){
+    digitalWrite(lightOutput, LOW);
+  }
+  else{
+    digitalWrite(lightOutput, HIGH);
+  }
+}
